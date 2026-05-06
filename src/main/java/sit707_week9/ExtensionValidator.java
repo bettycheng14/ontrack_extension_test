0fa495097c67723950b29e97abddb77edb224581
betty cheng <yycbetty14@gmail.com>
2026-05-06 14:34:49 +1000

    initial commit - red test

---
+++ src/main/java/sit707_week9/ExtensionValidator.java
@@ -0,0 +1,11 @@
+package sit707_week9;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class ExtensionValidator {
+	    
+    public static String submitRequest(String reason, LocalDate dueDate, LocalDate requestedDate) {
+
+    }
+}
---
+++ src/test/java/sit707_week9/ExtensionRequestTest.java
@@ -0,0 +1,72 @@
+package sit707_week9;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.time.LocalDate;
+
+public class ExtensionRequestTest {
+
+    // Helper baseline dates for testing
+    LocalDate dueDate = LocalDate.of(2026, 05, 07);
+
+    @Test
+    public void testValidExtensionRequest() {
+        String testName = "testValidExtensionRequest";
+        String reason = "I have been severely ill this week.";
+        LocalDate requestedDate = dueDate.plusDays(5);
+        
+        String status = ExtensionValidator.submitRequest(reason, dueDate, requestedDate);
+        
+        System.out.println(testName + " | Params: [" + reason + ", " + dueDate + ", " + requestedDate + "] | Returned: " + status);
+        
+        Assert.assertEquals("Submission success", status);
+    }
+
+    @Test
+    public void testReasonEmpty() {
+        String testName = "testReasonEmpty";
+        String reason = "";
+        LocalDate requestedDate = dueDate.plusDays(2);
+        
+        String status = ExtensionValidator.submitRequest(reason, dueDate, requestedDate);
+        
+        System.out.println(testName + " | Params: [" + reason + ", " + dueDate + ", " + requestedDate + "] | Returned: " + status);
+        Assert.assertEquals("You must enter a reason", status);
+    }
+
+    @Test
+    public void testReasonTooShort() {
+        String testName = "testReasonTooShort";
+        String reason = "More time plz";
+        LocalDate requestedDate = dueDate.plusDays(2);
+        
+        String status = ExtensionValidator.submitRequest(reason, dueDate, requestedDate);
+        
+        System.out.println(testName + " | Params: [" + reason + ", " + dueDate + ", " + requestedDate + "] | Returned: " + status);
+        Assert.assertEquals("The reason must be at least 15 characters long", status);
+    }
+
+    @Test
+    public void testDateBeforeDueDate() {
+        String testName = "testDateBeforeDueDate";
+        String reason = "I need more time to finish this.";
+        LocalDate requestedDate = dueDate.minusDays(1);
+        
+        String status = ExtensionValidator.submitRequest(reason, dueDate, requestedDate);
+        
+        System.out.println(testName + " | Params: [" + reason + ", " + dueDate + ", " + requestedDate + "] | Returned: " + status);
+        Assert.assertEquals("Due date too early", status);
+    }
+
+    @Test
+    public void testDateBeyondSevenDays() {
+        String testName = "testDateBeyondSevenDays";
+        String reason = "I need more time to finish this.";
+        LocalDate requestedDate = dueDate.plusDays(8);
+        
+        String status = ExtensionValidator.submitRequest(reason, dueDate, requestedDate);
+        
+        System.out.println(testName + " | Params: [" + reason + ", " + dueDate + ", " + requestedDate + "] | Returned: " + status);
+        Assert.assertEquals("Due date too late", status);
+    }
+}
